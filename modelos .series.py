7fc581a80af361a31839950bbcf3276066f8763f
milenainiguez07-maker <milenainiguez07@gmail.com>
2026-09-06 14:01:41 -0300

    docs/tp1.md

---
+++ modelos .series.py
@@ -0,0 +1,34 @@
+class Serie:
+    def __init__(self, titulo, genero, rating, anio, temporadas):
+        self._titulo = titulo
+        self._genero = genero
+        self._rating = rating
+        self._año = año
+        self._temporadas = temporadas
+
+    @property
+    def titulo(self):
+        return self._titulo
+
+    @property
+    def genero(self):
+        return self._genero
+
+    @property
+    def rating(self):
+        return self._rating
+
+    @property
+    def año(self):
+        return self._año
+
+    @property
+    def temporadas(self):
+        return self._temporadas
+
+    def __repr__(self):
+        return (
+            f"{self._titulo} ({self._genero}) "
+            f"⭐{self._rating} - {self._año} - "
+            f"{self._temporadas} temporadas"
+        )
