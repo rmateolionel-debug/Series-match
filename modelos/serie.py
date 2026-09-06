43414bb8430ec1a2d7c82686ac430d1eed15718c
Mateo Lionel Rodriguez Luque <rmateolionel@gmail.com>
2026-09-06 14:14:40 -0300

    Fix variable name from 'año' to 'anio'

--- modelos/serie.py
+++ modelos/serie.py
@@ -1,9 +1,9 @@
 class Serie:
-    def __init__(self, titulo, genero, rating, año, temporadas):
+    def __init__(self, titulo, genero, rating, anio, temporadas):
         self._titulo = titulo
         self._genero = genero
         self._rating = rating
-        self._año = año
+        self._anio = anio
         self._temporadas = temporadas
 
     @property
@@ -20,7 +20,7 @@ class Serie:
 
     @property
     def año(self):
-        return self._año
+        return self._anio
 
     @property
     def temporadas(self):
@@ -29,5 +29,5 @@ class Serie:
     def __repr__(self):
         return (
             f"{self._titulo} ({self._genero}) "
-            f"⭐{self._rating} - {self._año} - "
+            f"⭐{self._rating} - {self._anio} - "
             f"{self._temporadas} temporadas"   )
